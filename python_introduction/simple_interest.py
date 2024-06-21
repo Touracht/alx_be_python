837264d3518a28bb36aeb99436158345af427ec1
Touracht <tebogomataba6810@gmail.com>
2024-06-21 23:02:54 +0200

    Add simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,11 +2,7 @@
 principal = 1000
 rate = 0.05
 time = 3
-P = principal
-R = rate
-T = time
-I = P * R * T
-I = "interest"
+interest = principal * rate * time
 interest = "The simple interest is: 150.0"
 print(interest)
 #
