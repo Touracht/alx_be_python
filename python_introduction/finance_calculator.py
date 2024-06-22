beb4a5e42b4edaac45ddd8c0e145b25cf8063fa2
Touracht <tebogomataba6810@gmail.com>
2024-06-22 03:21:34 +0200

    Add finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,7 @@ input("Enter your monthly income:")
 input("Enter your total monthly expenses:")
 monthly_income = 5000
 monthly_expenses = 4000
-monthly_savings = 1000
-Projected_Savings = (1000 * 12 + (1000 * 12 * 0.05))
+monthly_savings = monthly_income - monthly_expenses
+Projected_Savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 print(Projected_Savings)
 #
