431a71ad4e9f674f6a61873d91b73c57f162690d
Touracht <tebogomataba6810@gmail.com>
2024-06-29 22:28:04 +0200

    Added multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,7 @@
 #
 number = int(input("Enter a number to see its multiplication table: "))
 
-for s in range(1, 10 + 1):
+for s in range(1, 11 + 1):
     print(number, "*" ,s, "=", number * s)
     #
     
