0074ad4e3c272f35dfbee618bd7433e23212c7e3
Touracht <tebogomataba6810@gmail.com>
2024-07-06 21:49:55 +0200

    Added fns_and_dsa package to the repository

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,20 @@
+#
+def perform_operation(num1, num2, operation):
+    """Calls for a function and
+      give output based on the chosen one"""
+    if operation == "add":
+       sum = num1 + num2       
+       return sum
+    
+    if operation == "subtract":
+        difference = num1 - num2
+        return difference
+    if  operation == "multiply":
+        product = num1 * num2
+        return product
+    if operation == "divide":
+        quotient = num1 / num2
+        if quotient == num1/0:
+            print("Cannot divide by zero!")
+        return quotient     
+#
