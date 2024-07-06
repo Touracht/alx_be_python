1049908663bfe9856e8b02980bff0e36370338e7
Touracht <tebogomataba6810@gmail.com>
2024-07-06 21:56:45 +0200

    Added fns_and_dsa package to the repository

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -14,8 +14,8 @@ def perform_operation(num1, num2, operation):
         return product
     if operation == "divide":
         quotient = num1 / num2
-        if num1 / 0 == 0:
-            print("Cannot divide by 0!")
+    elif num1 / 0 == 0:
+        print("Cannot divide by 0!")
         
         return quotient     
 #
