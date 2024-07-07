0a27293fc97f5e9106c153277405774c126f98a2
Touracht <tebogomataba6810@gmail.com>
2024-07-07 23:57:38 +0200

    Added temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,25 +2,23 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    
+    celsius = (fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR) - 32
+    return round(celsius, 1)
 
-def convert_to_fahrenheit(celsius):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    return (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+def convert_to_fahrenheit(celsius):   
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return round(fahrenheit, 1)
     
-
 def main():
     try:    
         temperature = float(input("Enter the temperature to convert: "))
         unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
         if unit == 'C':
             converted_temperature = convert_to_celsius(temperature)
-            print(f"{temperature}°F is {converted_temperature}°C")
+            print(f"{temperature}°C is {converted_temperature}°F")
         elif unit == 'F':
             converted_temperature = convert_to_fahrenheit(temperature)
-            print(f"{temperature}°C is {converted_temperature}°F")
+            print(f"{temperature}°F is {converted_temperature}°C")
         else:
             print("Invalid unit entered. Enter celsius of fahrenheit.")
     except ValueError as error:
