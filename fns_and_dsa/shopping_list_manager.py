57620f2c278b1919c2d99494cb95798cf1e8ba35
Touracht <tebogomataba6810@gmail.com>
2024-07-07 14:16:54 +0200

    Added shopping_list_manager.py to fns_and_dsa

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1' :
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{shopping_list} has been added to the list.")
             # Prompt for and add an item
