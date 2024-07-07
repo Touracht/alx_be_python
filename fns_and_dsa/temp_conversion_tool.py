58b9eed72f4f6b6055f73428cf494fc3f847865d
Touracht <tebogomataba6810@gmail.com>
2024-07-08 00:12:51 +0200

    Added explore_datetime.py to fns_and_dsa

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,7 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
-    celsius = (fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR) - 32
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return round(celsius, 1)
 
 def convert_to_fahrenheit(celsius):   
