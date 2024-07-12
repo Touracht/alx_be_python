8a6fe85cacbb9aa611f0d14c24482da7f062dbf7
Touracht <tebogomataba6810@gmail.com>
2024-07-12 16:24:02 +0200

    Added robust_division_calculator and main.py python files to programming_paradigm directory

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,13 +3,13 @@ def safe_divide(numerator, denominator):
 
     try:
         if division:
-            print(f"The result of the division is {division}")
+            print(f"The result of the division is {division:.2f}")
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     
     try:
         if division:
-            print(f"{numerator} / {denominator} is {division}")
+            print(f"{numerator} / {denominator} is {division:.2f}")
     except ValueError:
         print("Error: Please enter numeric values only.")
                   
