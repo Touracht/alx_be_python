27f1eb7f9d0be79096892734e7ce44825df513ee
Touracht <tebogomataba6810@gmail.com>
2024-07-13 23:55:55 +0200

    Added library_management and main2.py python files to programming_paradigm directory

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,8 +1,8 @@
-class book:
+class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        _is_checked_out = False
+        self._is_checked_out = False
 
 class Library:
     def __init__(self):
@@ -15,15 +15,4 @@ class Library:
         return adding_book
     
     def check_out_book(self, title):
-        if title == book:
-            return True
-        else:
-            return False
-    def return_book(self, title):
-        if title == book:
-            return False
-    def list_available_books(self):
-        for book in self._book:
-            return book
-
-    
+        
