96646e313396917756e114c33362a7c7e1a9831d
Touracht <tebogomataba6810@gmail.com>
2024-07-14 19:36:51 +0200

    Added library_management and main2.py python files to programming_paradigm directory

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,33 +3,36 @@ class Book:
         self.title = title
         self.author = author
         self._is_checked_out = False
-        self._is_returned = False
+    
+    def check_out(self):
+        if self._is_checked_out == False:
+            self._is_checked_out = True
 
-    def check_Out(self):
-        self._is_checked_out = False
-        return self._is_checked_out
     def return_book(self):
-        self._is_returned = False
+        if self._is_checked_out:
+            self._is_checked_out = False
 
 class Library:
     def __init__(self):
         self._book = []
         """Stores the books"""
 
-    def add_book(self, book):
-        """Add the book to the list"""
-        
-        adding_book = Book(self._book.append(book))
-        return adding_book
+    def add_book(self, Book):
+        """Add the book to the list"""  
+        self._book.append(Book)
     
-    def check_out_book(self, title):
-       for book in self._book:
-           if title == book:
-               return book
+    # def check_out_book(self, title):
+    #    for book in self._book:
+    #         if title == book
+           
     def return_book(self, title):
         for book in self._book:
             if title == book:
                 return book
+    
+    def list_available_books(self):
+        for book in self._book:
+            print(f"{book.title} by {book.author}")
             
             
 
--- programming_paradigm/main2.py
+++ programming_paradigm/main2.py
@@ -10,15 +10,15 @@ def main():
     print("Available books after setup:")
     library.list_available_books()
 
-    # Simulate checking out a book
-    library.check_out_book("1984")
-    print("\nAvailable books after checking out '1984':")
-    library.list_available_books()
+    # # Simulate checking out a book
+    # library.check_out_book("1984")
+    # print("\nAvailable books after checking out '1984':")
+    # library.list_available_books()
 
-    # Simulate returning a book
-    library.return_book("1984")
-    print("\nAvailable books after returning '1984':")
-    library.list_available_books()
+    # # Simulate returning a book
+    # library.return_book("1984")
+    # print("\nAvailable books after returning '1984':")
+    # library.list_available_books()
 
 if __name__ == "__main__":
     main()
