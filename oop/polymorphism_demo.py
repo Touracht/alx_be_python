cf606af0b3c96ee31b8e5eb945258e437b3f9b9e
Touracht <tebogomataba6810@gmail.com>
2024-07-19 22:37:37 +0200

    Add polymorphism_demo.py to oop directory

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -2,12 +2,12 @@ class Shape:
     def area(self):
         raise NotImplementedError(print("Derived classes need to override this method."))
     
-class Rectangle:
+class Rectangle(Shape):
     
     def area(self, length, width):
         return length * width
     
-class Circle:
+class Circle(Shape):
     def area(self, radius):
         import math
         return math.pi * (radius * radius)
