e55575cc8b5c867fcdc8a051a77a8820c3b851c4
Touracht <tebogomataba6810@gmail.com>
2024-07-19 17:54:05 +0200

    Added Library_system.py to oop directory

--- oop/library_system.py
+++ oop/library_system.py
@@ -32,7 +32,7 @@ class Library:
     def list_books(self):
         def __str__(self):
             for book in self.books:
-                return f"{Book.book}"
+                return f"{book}"
 
     
   
